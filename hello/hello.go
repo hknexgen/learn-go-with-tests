5ea4b534b0c2b3aea8a85927a60d1aef43f330bd
Hari Kiran <hknexgen@gmail.com>
2023-04-22 15:04:05 +0530

    Refactor Hello, world

--- hello/hello.go
+++ hello/hello.go
@@ -14,13 +14,19 @@ func Hello(name string, language string) string {
 	if name == "" {
 		name = "world"
 	}
-	prefix := englishHelloPrefix
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
 
 	switch language {
 	case french:
 		prefix = frenchHelloPrefix
 	case spanish:
 		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	return prefix + name
+	return prefix
 }
