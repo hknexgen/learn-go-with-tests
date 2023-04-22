65776b8a7d6840d74f470072f8a1ff7af488a7e4
Hari Kiran <hknexgen@gmail.com>
2023-04-22 14:51:32 +0530

    Add support for Hello World in Spanish

--- hello/hello.go
+++ hello/hello.go
@@ -1,8 +1,17 @@
 package main
 
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+
 func main() {
 }
 
-func Hello(name string) string {
-	return "Hello, " + name
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "world"
+	}
+	if language == "Spanish" {
+		return spanishHelloPrefix + name
+	}
+	return englishHelloPrefix + name
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,8 +3,29 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("Saying Hello to people", func(t *testing.T) {
+		got := Hello("Chris", "English")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Say 'Hello, world' when an empty string is passed", func(t *testing.T) {
+		got := Hello("", "English")
+		want := "Hello, world"
+
+		assertCorrectMessage(t, got, want)
+
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Nancy", "Spanish")
+		want := "Hola, Nancy"
+		assertCorrectMessage(t, got, want)
+	})
+
+}
+
+func assertCorrectMessage(t *testing.T, got string, want string) {
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
